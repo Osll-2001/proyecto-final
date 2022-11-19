2c0168ed3d0d71c23b27b7fd722bb071ba54b44f
octavio <octaviosuriellopezlopez@gmail.com>
2022-11-19 14:36:53 -0600

    Se agrego todo

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="capaLogin">
+      <form className='formLogin'>
+        <h1 className='titleForm'>Login</h1>
+        <div className="sectionForm">
+        <label htmlFor="User">User | Email</label>
+        <input type="text" id="User" placeholder='ExampleUser'/>
+        </div>
+        <div className="sectionForm">
+        <label htmlFor="Pass">Password</label>
+        <input type="password" id="Pass" placeholder='************'/>
+        </div>
+        <div className='sectiomForm'>
+        <button className='btnSignIn'>Sign In</button>
+        </div>
+      </form>
+      </div>
+      <a className='signUp'>Sign up</a>
+    </>
+  )
+}
+
+export default App
