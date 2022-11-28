97dcc37bd055c8e6577e45f90bbcfcb5b620e238
octavio <octaviosuriellopezlopez@gmail.com>
2022-11-27 21:31:14 -0600

    Redirección / a /home

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Error404 from '../pages/Error404'
 import Login from '../pages/Login'
 import SignUp from '../pages/SignUp'
+import Home from '../pages/Home'
 
 const RouteIndex = () => {
   return (
     <Routes>
+      <Route path='/home' element={<Home />} />
+      {/* Redireccionar el path / para que vaya siempre a /home */}
+      <Route path='/' element={<Navigate to='/home' replace />} />
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
       <Route path='*' element={<Error404 />} />
