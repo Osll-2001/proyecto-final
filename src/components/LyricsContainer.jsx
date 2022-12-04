691612e8ff2fac53389c9e9822f21d95ab24f9b8
octavio <octaviosuriellopezlopez@gmail.com>
2022-12-03 21:55:29 -0600

    Condicional rending a Container de lyrics

--- src/components/LyricsContainer.jsx
+++ src/components/LyricsContainer.jsx
@@ -8,23 +8,33 @@ const LyricsContainer = () => {
   return (
     <div className='containerLyrics'>
       {context.searchedSong === undefined
-        ? <h1 className=''>Canción no encontrada</h1>
+        ? <>
+          <h1>:(</h1>
+          <div className='lyricsMusic'>
+            <textarea name='txtALyrics' id='txtALyrics' value='La Canción No Ha Encuentrado, Verifique El Nombre De La Banda O Canción' readOnly />
+          </div>
+          </>
         : context.searchedSong._id
           ? <>
             <div className='infoMusic'>
-              <h5><span className='info-title'>Nombre</span>{context.searchedSong.name}</h5>
-              <h5><span className='info-title'>Banda</span>{context.searchedSong.band}</h5>
-              <h5><span className='info-title'>Album</span>{context.searchedSong.album}</h5>
-              <h5><span className='info-title'>Año</span>{context.searchedSong.releaseYear}</h5>
-              <h5><span className='info-title'>Duración</span>{context.searchedSong.length}</h5>
-              <h5><span className='info-title'>Genero</span>{context.searchedSong.genre}</h5>
-              <a href={context.searchedSong.link} target='blank'>Escuchar</a>
+              <p><span className='info-title'>Nombre</span>{context.searchedSong.name}</p>
+              <p><span className='info-title'>Banda</span>{context.searchedSong.band}</p>
+              <p><span className='info-title'>Album</span>{context.searchedSong.album}</p>
+              <p><span className='info-title'>Año</span>{context.searchedSong.releaseYear}</p>
+              <p><span className='info-title'>Duración</span>{context.searchedSong.length}</p>
+              <p><span className='info-title'>Genero</span>{context.searchedSong.genre}</p>
+              <button className='btnSong'><a href={context.searchedSong.link} target='blank' className='linkSong'>Escuchar</a></button>
             </div>
             <div className='lyricsMusic'>
               <textarea name='txtALyrics' id='txtALyrics' value={context.searchedSong.lyrics} readOnly />
             </div>
-            </>
-          : <h1>Busque una Canción</h1>}
+          </>
+          : <>
+            <h1>:)</h1>
+            <div className='lyricsMusic'>
+              <textarea name='txtALyrics' id='txtALyrics' value='Busca Una Canción, Especificando El Nombre Y La Banda O Artista' readOnly />
+            </div>
+          </>}
     </div>
   )
 }
