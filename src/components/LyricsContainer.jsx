90db74a5017c75ccc9395378623c601e9753ecc3
octavio <octaviosuriellopezlopez@gmail.com>
2022-12-03 18:44:09 -0600

    Implementación llamada Api y impresión en pantalla

--- src/components/LyricsContainer.jsx
+++ src/components/LyricsContainer.jsx
@@ -1,19 +1,30 @@
+import { Link } from 'react-router-dom'
+import { useSongContext } from '../context/SongContext'
 import '../styles/LyricsContainer.css'
 
 const LyricsContainer = () => {
+  const context = useSongContext()
+
   return (
     <div className='containerLyrics'>
-      <div className='infoMusic'>
-        <h5>Name</h5>
-        <h5>Band</h5>
-        <h5>Album</h5>
-        <h5>Year</h5>
-        <h5>leght</h5>
-        <h5>Genre</h5>
-      </div>
-      <div className='lyricsMusic'>
-        <textarea name='txtALyrics' id='txtALyrics' value='Lyrics' readOnly />
-      </div>
+      {context.searchedSong === undefined
+        ? <h1 className=''>Canción no encontrada</h1>
+        : context.searchedSong._id
+          ? <>
+            <div className='infoMusic'>
+              <h5><span className='info-title'>Nombre</span>{context.searchedSong.name}</h5>
+              <h5><span className='info-title'>Banda</span>{context.searchedSong.band}</h5>
+              <h5><span className='info-title'>Album</span>{context.searchedSong.album}</h5>
+              <h5><span className='info-title'>Año</span>{context.searchedSong.releaseYear}</h5>
+              <h5><span className='info-title'>Duración</span>{context.searchedSong.length}</h5>
+              <h5><span className='info-title'>Genero</span>{context.searchedSong.genre}</h5>
+              <a href={context.searchedSong.link} target='blank'>Escuchar</a>
+            </div>
+            <div className='lyricsMusic'>
+              <textarea name='txtALyrics' id='txtALyrics' value={context.searchedSong.lyrics} readOnly />
+            </div>
+            </>
+          : <h1>Busque una Canción</h1>}
     </div>
   )
 }
--- src/components/NavHome.jsx
+++ src/components/NavHome.jsx
@@ -1,6 +1,7 @@
 import { useSongContext } from '../context/SongContext'
 import { useState } from 'react'
 import '../styles/NavHome.css'
+import axios from 'axios'
 
 const NavHome = () => {
   const context = useSongContext()
@@ -13,13 +14,19 @@ const NavHome = () => {
     setDataSearched({ ...dataSearched, [name]: value })
   }
 
+  const searchSong = async () => {
+    const url = `https://api-devf.up.railway.app/songs?name=${dataSearched.song}&band=${dataSearched.artist}`
+    const response = await axios.get(url)
+    context.setSearchedSong(response.data.data[0])
+  }
+
   return (
     <nav className='NavHome'>
       <div className='elementos' />
       <div className='buscador'>
         <input type='text' value={dataSearched.artist} name='artist' id='inputArtista' className='inputBus' placeholder='Artista' onChange={handleData} />
         <input type='text' value={dataSearched.song} name='song' id='inputCancion' className='inputBus' placeholder='Canción' onChange={handleData} />
-        <button className='btnBuscar' onClick={() => { context.setSearchedSong(dataSearched) }}>Buscar</button>
+        <button className='btnBuscar' onClick={searchSong}>Buscar</button>
       </div>
     </nav>
   )
