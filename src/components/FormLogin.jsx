0f58d01b1652d673c0c7ccc4aa2cbbfb21f71dee
octavio <octaviosuriellopezlopez@gmail.com>
2022-12-09 00:00:40 -0600

    Implementación parcial de Login

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,22 +1,57 @@
-import React from 'react'
 import '../styles/Login-Register.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import { useState } from 'react'
+import axios from 'axios'
 
 const Login = () => {
+  const [credentials, setCredentials] = useState({
+    email: '',
+    pass: ''
+  })
+
+  const [invalidCred, setInvalidCred] = useState(false)
+  const navigate = useNavigate()
+
+  const handleData = ({ target: { value, name } }) => {
+    setCredentials({ ...credentials, [name]: value })
+  }
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const user = await axios.get(`https://api-music.up.railway.app/users?email=${credentials.email}&pass=${credentials.pass}`)
+      // si la data tiene array entonces significa que si encontro al usuario
+      if (user.data.data.length) {
+        // Lo redirige a esa ruta
+        navigate('/homeadmin')
+      } else {
+        // Activa este State para que se muestra el Error de Datos Invalidos
+        setInvalidCred(true)
+        // Se agrego para que este letrero de Datos Invalidos dure 3 segundos en estar
+        setTimeout(() => {
+          setInvalidCred(false)
+        }, 3000)
+      }
+    } catch (err) {
+      throw Error('Error: ', err)
+    }
+  }
+
   return (
     <>
       <form className='formLogin'>
         <h1 className='titleForm'>Login</h1>
         <div className='sectionForm'>
-          <label htmlFor='User'>User | Email</label>
-          <input className='inputForm' type='text' id='User' placeholder='example@example.com' />
+          <label htmlFor='email'>User | Email</label>
+          <input className='inputForm' type='text' id='email' name='email' value={credentials.email} onChange={handleData} placeholder='example@example.com' />
         </div>
         <div className='sectionForm'>
-          <label htmlFor='Pass'>Password</label>
-          <input className='inputForm' type='password' id='Pass' placeholder='' />
+          <label htmlFor='pass'>Password</label>
+          <input className='inputForm' type='password' id='pass' name='pass' value={credentials.pass} onChange={handleData} placeholder='' />
         </div>
+        {invalidCred ? <div className='sectionForm invalidCred'>Datos Incorrectos</div> : <></>}
         <div className='sectionForm'>
-          <button className='btnSign'>Sign In</button>
+          <button className='btnSign' onClick={onSubmit}>Sign In</button>
         </div>
         <div className='sectionForm sectionQuestion'>
           <span className='questionAccount'>Don't have an account? </span><Link className='loginLink' to='/signup'> Sign up</Link>
